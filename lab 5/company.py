5ba5e36408947bca7194a1f584aefc7bef2e8475
Amritiiit <amritsagar3210@gmail.com>
2024-11-20 08:30:53 +0530

    Functions

--- lab 5/company.py
+++ lab 5/company.py
@@ -1,10 +1,9 @@
-names=input("ENTER THE NAME OF 5 EMPLOYEES : ").split()
-salary=list(map(int,input("ENTER THE SALARIES OF 5 EMPLOYEES : ").split()))
-employee={names[i]:salary[i] for i in range(len(names))}
-for i in range(len(salary)):
-    for j in range(len(salary)-1):
-        if salary[j]>salary[j+1]:
-            salary[j],salary[j+1]=salary[j+1],salary[j]
-            names[j],names[j+1]=names[j+1],names[j]
-for i in range(len(names)):
-    print(f"{i+1}. {names[i]} : Rs. {salary[i]}")
+for _ in range(int(input())):
+    n,q=map(int,input().split())
+    p=list(map(int,input().split()))
+    s=input()
+    maxi=float('-inf')
+    mini=float('inf')
+    for i in range(n-1):
+        if 
+    
